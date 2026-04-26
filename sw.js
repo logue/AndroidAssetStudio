aa9c91916314f95ca16fb34763ac1346534907d3
logue <logue@hotmail.co.jp>
2026-04-26 01:41:23 +0000

    Deploying to gh-pages from @ logue/AndroidAssetStudio@bd1be8eb4c2d67933d349183fa602d2c6ca4a9d7 🚀

--- sw.js
+++ sw.js
@@ -1,27 +1,46 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { cacheFirst, staleWhileRevalidate } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { initialize } from 'workbox-google-analytics';
 
-precacheAndRoute([{"revision":"f3d7b3efa180da08ce3d6319443e39d4","url":"icons-actionbar.html"},{"revision":"bdb88519a699b9d08592b43140c23bc1","url":"icons-app-shortcut.html"},{"revision":"2991d5228886bef6f440e1433a8de655","url":"icons-generic.html"},{"revision":"21e9ffeb0b70b006683e23a9056f518c","url":"icons-launcher.html"},{"revision":"71d03ab473142a6a9db0ffc740791513","url":"icons-notification.html"},{"revision":"8e6407fc8d7f9621e4893a6800f59852","url":"index.html"},{"revision":"c5baeb32e240b567a146ffeefcdc779e","url":"nine-patches.html"},{"revision":"65311bbd1a2658cacdf6a2be539b0d9c","url":"res/generator-thumbs/icon-animator.svg"},{"revision":"747ac6e1b23e6f00a86d7baebe76029d","url":"res/generator-thumbs/icons-actionbar.svg"},{"revision":"dcd36cf4d4b734e4d4d7993aeb5350ce","url":"res/generator-thumbs/icons-app-shortcut.svg"},{"revision":"7e9aaa9edeaf210c7afac117cf094192","url":"res/generator-thumbs/icons-generic.svg"},{"revision":"ac624b8aabda5851413f3ccfd252b80d","url":"res/generator-thumbs/icons-launcher.svg"},{"revision":"bd07505811fade5e742afe6a85cedf03","url":"res/generator-thumbs/icons-notification.svg"},{"revision":"c37457a837ee23a6c1981b5d993ee72e","url":"res/generator-thumbs/nine-patches.svg"},{"revision":"0a2645500fa29272a964ab81aed08eae","url":"app.js"},{"revision":"04e16c151ff9421a5691a625535dbd98","url":"sw-prod.js"},{"revision":"1da48b55530b074a99a0753b32c89c78","url":"vendor.js"},{"revision":"aceec3b0ec989b4b5a37832a0e432c76","url":"app.css"}]);
+const CLIPART_NAMES_URL =
+  'https://raw.githubusercontent.com/marella/material-icons/refs/heads/main/_data/codepoints.json';
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js'
-);
+precacheAndRoute([{"revision":"da6f78b87e5e71eaa91241440cafb620","url":"nine-patches.html"},{"revision":"5061d511f3932e0aacf99ed970745a75","url":"index.html"},{"revision":"20c5f4c98f5430cb598cabed2436a91c","url":"icons-notification.html"},{"revision":"d2e0847df5e896f6e9088a81e4ab560f","url":"icons-launcher.html"},{"revision":"4e7a1f8f75d9118e0095c29acae212b7","url":"icons-generic.html"},{"revision":"e90018dd30417eee46b36c19710f47f9","url":"icons-app-shortcut.html"},{"revision":"b9ff965cc2a716f8b9a9fa987abf3b5d","url":"icons-actionbar.html"},{"revision":"246a0419730e8bfcf9641a2581759e49","url":"res/generator-thumbs/nine-patches.svg"},{"revision":"aa857754e748eae1c5048f35809d8919","url":"res/generator-thumbs/icons-notification.svg"},{"revision":"647f4c5df68ff1ced4dc6c31ab755efa","url":"res/generator-thumbs/icons-launcher.svg"},{"revision":"f59bf46c029a1772376f2a9a0cb5c0cd","url":"res/generator-thumbs/icons-generic.svg"},{"revision":"5fc9f2c7fbe7b7b8ecdc1e31c191dfbc","url":"res/generator-thumbs/icons-app-shortcut.svg"},{"revision":"2709d863f9bbff8e3429c603694e4aa7","url":"res/generator-thumbs/icons-actionbar.svg"},{"revision":"ac74b1df04d0c18384d556fe5f73cfa7","url":"res/generator-thumbs/icon-animator.svg"},{"revision":"aceec3b0ec989b4b5a37832a0e432c76","url":"app.css"}]);
 
-workbox.precaching.precacheAndRoute([]);
-
-workbox.routing.registerRoute(
-  new RegExp('https://(?:fonts|www).(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.cacheFirst({
-    cacheName: 'google-fonts',
+registerRoute(
+  ({ url }) => url.href === CLIPART_NAMES_URL,
+  staleWhileRevalidate({
+    cacheName: 'clipart-names',
     plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
+      new ExpirationPlugin({
+        maxEntries: 1,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
         purgeOnQuotaError: true,
       }),
-      new workbox.cacheableResponse.Plugin({
+      new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
     ],
   })
 );
 
-workbox.googleAnalytics.initialize();
+registerRoute(
+  new RegExp('https://(?:fonts|www).(?:googleapis|gstatic).com/(.*)'),
+  cacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 20,
+        purgeOnQuotaError: true,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+initialize();
